67863db45057f5e0d61bb3525027a3f76d14f77a
nati-natnael <natnaelger@gmail.com>
2022-01-21 01:08:05 -0600

    switch multi thread

--- src/main/main.py
+++ src/main/main.py
@@ -1,15 +1,24 @@
-import time
-
 import cv2
 import zmq
 import yaml
+import time
 import logging
-import multiprocessing
+import threading
 
 from time import sleep
+from queue import Queue
 from datetime import datetime
 
 logging.basicConfig(format="%(asctime)s %(threadName)-9s [%(levelname)s] - %(message)s", level=logging.DEBUG)
+
+
+queue = Queue(maxsize=20)
+
+
+def add_name(frame, name):
+    cv2.putText(frame, name, org=(10, 30),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
 
 def add_datetime_to(frame):
@@ -36,36 +45,32 @@ def load_config(config_file_path):
     return configuration
 
 
-def read_from_camera(cam_id, q):
+def read_from_camera(cam_id):
     capture = cv2.VideoCapture(cam_id)
 
     while True:
-        start_time = time.time()
-        _, frame = capture.read()
+        # start_time = time.time()
+        _, _frame = capture.read()
 
-        q.put(frame)
+        # # Display the resulting frame
+        # cv2.imshow('Read Frame', frame)
+        # if cv2.waitKey(25) & 0xFF == ord('q'):
+        #     breakd
+
+        queue.put(_frame)
 
         # sleep(Streamer.THREAD_SLEEP)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        # print("--- Read Frame: %s seconds ---" % (time.time() - start_time))
 
-
-def process_frame(q):
-    while True:
-        frame = q.get()
-        # Display the resulting frame
-        cv2.imshow('Frame', frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-
-        # _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
-        # socket.send(buffer)
+        # sleep(0.0005)
 
 
 if __name__ == '__main__':
     config = load_config("C:/Users/meti-nati/PycharmProjects/HomeSecuritySource/src/resources/application.yml")
     ip = config.get('ip')
     port = config.get('port')
-    camId = config.get('camera-id')
+    camId = config.get('camera_id')
+    camName = config.get('camera_name')
 
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
@@ -73,11 +78,31 @@ if __name__ == '__main__':
 
     print(f"Streaming to --> {ip}:{port}")
 
-    q = multiprocessing.Queue()
-    p1 = multiprocessing.Process(target=read_from_camera, args=(camId, q))
+    t1 = threading.Thread(target=read_from_camera, args=(camId, ))
+    t1.start()
 
-    p1.start()
+    while True:
+        start_time = time.time()
+        size = 0
 
-    p1.join()
+        if not queue.empty():
+            size = queue.qsize()
+
+            vid_frame = queue.get()
+
+            add_datetime_to(vid_frame)
+            add_name(vid_frame, camName)
+
+            _, buffer = cv2.imencode('.jpg', vid_frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
+            socket.send(buffer)
+
+            # # Display the resulting frame
+            # cv2.imshow('Process Frame', frame)
+            # if cv2.waitKey(25) & 0xFF == ord('q'):
+            #     break
+
+        print(f"--- Process Frame: Queue Size: {size}: {(time.time() - start_time)} seconds ---")
+
+        sleep(0.0005)
 
 
