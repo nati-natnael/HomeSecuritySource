24beb8ec2f57a645466543b8cd33a926f613a983
nati-natnael <natnaelger@gmail.com>
2022-08-29 01:14:20 -0500

    cleaning up

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ import argparse
 from streamer import Streamer
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Stream video source")
+    parser = argparse.ArgumentParser(description='Stream video source')
 
-    parser.add_argument("-i", "--ip", help="ip address", default="127.0.0.1")
-    parser.add_argument("-p", "--port", help="port number", type=int, default=8080)
-    parser.add_argument("-c", "--camera", help="camera id", type=int, default=0)
+    parser.add_argument('-i', '--ip', help='ip address', default='127.0.0.1')
+    parser.add_argument('-p', '--port', help='port number', type=int, default=8080)
+    parser.add_argument('-c', '--id', help='camera id', type=int, default=0)
+    parser.add_argument('-n', '--name', help='camera id', default='N/A')
 
     args = parser.parse_args()
 
-    streamer = Streamer(args.ip, args.port, args.camera)
+    streamer = Streamer(args.ip, args.port, args.id, args.name)
     streamer.start()
--- src/streamer.py
+++ src/streamer.py
@@ -6,26 +6,55 @@ from time import sleep
 from datetime import datetime
 from imutils.video import VideoStream
 
-logging.basicConfig(format="%(asctime)s %(threadName)-9s [%(levelname)s] - %(message)s", level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(threadName)-9s [%(levelname)s] - %(message)s', level=logging.DEBUG)
 
 
-def add_datetime(frame, camera_name="N/A"):
+def write_text_with_dark_outline(frame, text, text_color, position):
+    text_outline_color = (0, 0, 0)
+
+    cv2.putText(frame, f'{text}', org=position,
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                color=text_outline_color, thickness=5, lineType=cv2.LINE_AA)
+    cv2.putText(frame, f'{text}', org=position,
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                color=text_color, thickness=2, lineType=cv2.LINE_AA)
+
+
+def add_description(frame, camera_name='N/A'):
     if len(frame.shape) == 2:
         height, width = frame.shape
     else:
         height, width, _ = frame.shape
 
-    text_color = (0, 0, 255)
+    text_color = (255, 255, 255)
+    camera_name_position = (10, 30)
+    datetime_position = (10, height - 20)
 
-    cv2.putText(frame, f"Cam {camera_name}", org=(10, 30),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                color=text_color, thickness=2, lineType=cv2.LINE_AA)
+    write_text_with_dark_outline(frame, camera_name, text_color, camera_name_position)
 
-    datetime_string = datetime.now().strftime("%b %d, %Y %H:%M:%S")
+    datetime_string = datetime.now().strftime('%b %d, %Y %H:%M:%S')
 
-    cv2.putText(frame, datetime_string, org=(10, height - 20),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                color=text_color, thickness=2, lineType=cv2.LINE_AA)
+    write_text_with_dark_outline(frame, datetime_string, text_color, datetime_position)
+
+
+def send_frame(client_socket, server_addr, buffer):
+    padded_buffer_size = str(buffer.size).zfill(Streamer.DIGIT_COUNT)
+
+    msg_start = bytes(f'START,{padded_buffer_size}', 'utf-8')
+
+    # send message
+    client_socket.sendto(msg_start, server_addr)
+
+    buffer_bytes = buffer.tobytes()
+    for x in range(0, buffer.size, Streamer.MAX_MSG_SIZE):
+        start = x
+        end = start + Streamer.MAX_MSG_SIZE
+
+        if end > buffer.size:
+            end = buffer.size
+
+        send_buf = buffer_bytes[start:end]
+        client_socket.sendto(send_buf, server_addr)
 
 
 class Streamer:
@@ -34,45 +63,30 @@ class Streamer:
     DIGIT_COUNT = 8
     FRAME_SIZE = (480, 320)
 
-    def __init__(self, ip, port, camera_id):
+    def __init__(self, ip, port, camera_id, camera_name):
         self.ip = ip
         self.port = port
         self.camera_id = camera_id
+        self.camera_name = camera_name
 
     def start(self):
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         server_addr = (self.ip, self.port)
 
-        print(f"streaming to --> {server_addr}")
+        print(f'streaming to --> {server_addr}')
 
         vs = VideoStream(src=self.camera_id).start()
 
         while True:
             frame = vs.read()
 
-            add_datetime(frame)
+            add_description(frame, self.camera_name)
 
             frame = cv2.resize(frame, Streamer.FRAME_SIZE)
 
             _, buffer = cv2.imencode('.jpg', frame)
 
-            padded_buffer_size = str(buffer.size).zfill(8)
-
-            msg_start = bytes(f"START,{padded_buffer_size}", "utf-8")
-
-            # send message
-            client.sendto(msg_start, server_addr)
-
-            buffer_bytes = buffer.tobytes()
-            for x in range(0, buffer.size, Streamer.MAX_MSG_SIZE):
-                start = x
-                end = start + Streamer.MAX_MSG_SIZE
-
-                if end > buffer.size:
-                    end = buffer.size
-
-                send_buf = buffer_bytes[start:end]
-                client.sendto(send_buf, server_addr)
+            send_frame(client, server_addr, buffer)
 
             sleep(Streamer.THREAD_SLEEP)
